a19542b31e52e2a41fa4b8f79426fe1286f06801
Pavan K Rao <raopavank@gmail.com>
2025-09-12 01:09:06 +0530

    Fix wrong base path for deployed site

--- astrosite/astro.config.mjs
+++ astrosite/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://pavankrao-qlp.github.io/portfolio/',
+  base: '/portfolio/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
