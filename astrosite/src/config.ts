f3798b6458ee12425eb0ee48d26ed726381a9af2
Pavan K Rao <raopavank@gmail.com>
2026-03-15 17:53:29 +0530

    updated resume and more descriptive landing page

--- astrosite/src/config.ts
+++ astrosite/src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword. 
 
 export const SITE_TITLE = 'PAVAN K RAO';
-export const SITE_DESCRIPTION = 'Personal Portfolio Website built with Astro.';
+export const SITE_DESCRIPTION = 'Game Programmer with 8 years in Unreal Engine & Unity — ECS, multiplayer, shipped titles on Android & iOS.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
